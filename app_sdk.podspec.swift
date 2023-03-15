70b8ce555847719047cdff53de33d8e4e20b0407
bilal <bilal.oz@innovance.com.tr>
2023-03-15 12:15:33 +0300

    test

---
+++ app_sdk.podspec.swift
@@ -0,0 +1,14 @@
+Pod::Spec.new do |s|
+    s.name         = "App"
+    s.version      = "1.0.0"
+    s.summary      = "App: app"
+    s.description  = "Demo repository for framework distribution."
+    s.homepage     = "http://github.com"
+    s.license = { :type => "MIT", :file => "LICENSE" }
+    s.author             = { "Test" => "bbbb@gmail.com" }
+    s.source       = { :git => "https://github.com/bilaloz/App", :branch => "main", :tag => "#{s.version}" }
+    s.vendored_frameworks = "App.xcframework"
+    s.platform = :ios
+    s.swift_version = "5.5"
+    s.ios.deployment_target  = '12.0'
+end
